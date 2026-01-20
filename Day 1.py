48259f7912bd525280d59dca5a1d8adf09220e08
M6Jet <Mai6jeterbiz@gmail.com>
2026-01-20 14:55:44 -0500

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,23 @@
+# Variables
+# name = input("What is your name?")
+# print(name)
+
+# name = "Angela"
+# print(name)
+
+# len function
+# print(len(input("What is your name?")))
+# username = input("What is your name?")
+# length = len(username)
+# print(length)
+
+# naming variables
+# user_name = "Angela"
+# length = len(user_name)
+# print(length)
+
+# Project (Band Name Generator)
+# Print("Hello Welcome to the Band Name Generator")
+# Place = input("Where did tou grow up?\n")
+# Pet = input("What's the name of your pet?\n")
+# print("Your band name could be: " + city + " " + pet)
