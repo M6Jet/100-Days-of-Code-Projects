fece2d384adb9a705dc249ae1c72eb25688bdec7
M6Jet <Mai6jeterbiz@gmail.com>
2026-01-20 15:56:32 -0500

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,61 @@
+# Subscripting
+# print("Hello"[0])
+
+# String
+# print("123" + "345")
+
+# Integer = Whole number
+# print(123 + 345)
+
+# Large Integers
+# print(123456789)
+
+# Float = Floating Point Number
+# print(3.14159)
+
+# Boolean
+# print(True)
+# print(False)
+
+# Type Checking
+# print(type(123))
+
+# Type Conversion
+# print(int("123") + int("456"))
+
+# Mathmatical Operations
+# print("My age: " + str(12))
+# print(123 + 456)
+# print(7 - 3)
+# print(3 * 2)
+# print(6 / 3)
+# print(6 // 3)
+# print(2**2)
+
+# bmi = 84 / 1.65 ** 2
+# print(bmi)
+# print(int(bmi))
+# print(round(bmi))
+# print(round(bmi, 2))
+
+# score = 0
+# height = 1.8
+# is_winning = True
+# User scores a point
+# score += 1
+# print(score)
+
+# f-strings
+# print(f"Your score is = {score}, your height {height}. You are winning is {is_winning}")
+
+# Tip Calculator Project
+# print("Welcome to the tip calculator!")
+# bill = float(input("what was the total bill? $"))
+# tip = int(input("What percentage tip would you like to give? 10 12 15"))
+# people = int(input("How many people to split the bill? "))
+# tip_as_percent = tip / 100
+# total_tip_amount = bill * tip_as_percent
+# total_bill = bill + total_tip_amount
+# bill_per_person = total_bill / people
+# final_amount = round(bill_per_person, 2)
+# print(f"Each person should pay ${final_amount}")
