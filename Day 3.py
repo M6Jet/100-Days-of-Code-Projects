59d5aef4ad39708aba3a97fe948509fadaf26e2c
M6Jet <Mai6jeterbiz@gmail.com>
2026-01-21 11:27:54 -0500

    Add files via upload

---
+++ Day 3.py
@@ -0,0 +1,92 @@
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm? "))
+
+# if height >= 120:
+#     print("You can ride the rollercoaster")
+# else:
+#     print("Sorry you have to grow taller before you can ride")
+
+# Even number 12 % 2 == 0
+# number = print(10 % 3)
+# if number == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+# if height >= 120:
+#     print("You can ride the rollercoaster")
+#     age = int(input("What is your age?"))
+#     if age <=12:
+#         print("Child Tickets are $5")
+#         bill = 5
+#     elif age <= 18:
+#         print("Youth Tickets are $7")
+#         bill = 7
+#     else:
+#         bill = 12
+#         print("Adult tickets are $12")
+
+# wants_photo = input("Do you want a photo ticket? Type y for Yes and n for No")
+# if wants_photo == "y":
+#     Add $3 to their bill
+# bill += 3
+
+# print(f"Your final bill is ${bill} ")
+# else:
+#     print("Sorry you have to grow taller before you can ride.")
+
+# print("Welcome to Python Pizza Deliveries")
+# size = input("What size pizza do you want? S, M, or L:")
+# pepperoni = input("Do you want pepperoni on your pizza? Y or N:")
+# extra_cheese = input("Do you want extra cheese? Y or N")
+# if size == "S":
+#     print("Small pizza is $15")
+#     bill += 15
+# elif size == "M":
+#     print("Medium pizza is $20")
+#     bill += 20
+# elif size == "L":
+#     print("Large pizza is $20")
+#     bill += 25
+# else:
+#     print("You typed the wrong inputs.")
+
+# if pepperoni == "Y":
+#     if size == "S":
+#         bill += 2
+#     else:
+#         bill += 3
+
+# if extra_cheese == "Y"
+#     bill += 1
+
+# print(f"Your final bill is ${bill}.")
+
+# Project Treasure Island
+# print("Welcome to Treasure Island.")
+# print("Your mission is to find the treasure.")
+# choice1 = input('You\'re at a crossroad, where do you want to go? Type "left" or "right" ').lower()
+
+# if choice1 == "left":
+#     choice2 = input('You\'ve come to a lake, there is an island in the middle of the lake.'
+#                     'Type "wait" to wait for a boat. Tupe "swim" to swim across. ').lower()
+
+    # if choice2 == "wait":
+    #     choice3 = input("You arrive at the island unharmed."
+    #                     "There is house with 3 doors. One red,"
+    #                     "one yellow and one blue."
+    #                     "Which color do you choose?").lower()
+    #     if choice3 == "red":
+    #         print("It's a room full of fire. Game Over")
+    #     elif choice3 == "yellow":
+    #         print("You found the treasure. You Win!")
+    #     elif choice3 == "blue":
+    #         print("You enter a room of beasts. Game Over.")
+    #     else:
+    #         print("You chose a door that doesn't exist. Game Over")
+
+    # else:
+    #     print("You got attacked by an angry trout. Game Over.")
+
+# else:
+#     print("You fell in to a hole. Game Over.")
