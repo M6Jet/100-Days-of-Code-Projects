4ab2bc5e7a5f41bdc7acfb11b0eccf2aaf2c625e
M6Jet <Mai6jeterbiz@gmail.com>
2026-01-22 20:23:14 -0500

    Add files via upload

---
+++ Day 4.py
@@ -0,0 +1,47 @@
+# import random
+import random
+
+# random_integer = random.randint(1,10)
+# print(random_integer)
+
+# random_number_0_to_1 = random.random()
+# print(random_number_0_to_1)
+
+# random_float = random.uniform(1, 10)
+# print(random_float)
+
+# random_heads_or_tails = random.randint(0,1)
+# if random_heads_or_tails == 0:
+#     print("Heads")
+# else:
+#     print("Tails")
+
+# states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia"]
+
+# print(states_of_america[1]) = "Pencilvania"
+
+# states_of_america.append("Angelaland")
+
+# friends = ["Alice", "Bob", "Charlie","David"]
+
+# print(random.choice(friends))
+
+# random_index = random.randint(0, 4)
+# print(friends[random_index])
+
+# user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors"))
+
+# computer_choice = random.randint(0, 2)
+# print(f"Computer Chose {computer_choice}")
+# if user_choice == 0 and computer_choice == 2:
+#     print("You win!")
+# elif computer_choice == 0 and user_choice == 2:
+#     print("You lose!")
+# elif computer_choice > user_choice:
+#     print("You lose!")
+# elif computer_choice == user_choice:
+#     print("It's a draw!")
+# elif user_choice >= 3 or user_choice < 0:
+#     print("You typed an invalid number. You lose!")
+
+
