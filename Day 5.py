5fd5012508726d1244e610fb464189384289db41
M6Jet <Mai6jeterbiz@gmail.com>
2026-01-26 16:16:03 -0500

    Add files via upload

---
+++ Day 5.py
@@ -0,0 +1,29 @@
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + " pie")
+# print(fruits)
+
+# student_scores = [150, 142, 185, 120, 171, 149, 146]
+
+# total_exam_score - sum(student_scores)
+
+# sum = 0
+# for score in student_scores:
+#     sum += score
+
+# print(sum)
+
+# student_scores = [150, 142, 185, 120, 171, 149, 146]
+
+# max_score = 0
+# for score in student_scores:
+#     if score > max_score:
+#         max_score = score
+
+# print(max_score)
+
+# range function with for loop
+
+# for number in range(1, 11, 3):
+#     print(number)
